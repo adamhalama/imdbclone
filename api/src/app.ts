a694374b08cfb698716443a2d43bfe58551836a3
janadamhalama <janadam.h@hotmail.sk>
2023-05-31 18:37:44 +0200

    Pipeline demonstration

--- api/src/app.ts
+++ api/src/app.ts
@@ -12,7 +12,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use('/health', (_, res) => res.status(HTTP_STATUS_CODES.OK).json("Service is healthy!!"))
+app.use('/health', (_, res) => res.status(HTTP_STATUS_CODES.OK).json("Service is healthy! Demonstration!"))
 
 app.use('/', (req, res, next) => tokenMiddleware(req, res, next))
 
